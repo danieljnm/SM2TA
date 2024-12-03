7e3a6a7357ca9852b3761a6add98c2a8b8f2df46
Daniel J. N.-Müller <daniel.m.jorgensen@gmail.com>
2024-12-03 13:21:15 +0100

    Init

---
+++ sm2ta/src/main/java/danieljnm/sm2ta/HelloXtend.xtend
@@ -0,0 +1,11 @@
+package danieljnm.sm2ta
+
+class HelloXtend {
+	
+	def static void main(String[] args) {
+		println(message)
+	}
+	
+	def static getMessage () '''Hello Xtend!'''
+	
+}
---
+++ sm2ta/target/xtend-gen/main/danieljnm/sm2ta/HelloXtend.java
@@ -0,0 +1,17 @@
+package danieljnm.sm2ta;
+
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
+@SuppressWarnings("all")
+public class HelloXtend {
+  public static void main(final String[] args) {
+    InputOutput.<CharSequence>println(HelloXtend.getMessage());
+  }
+
+  public static CharSequence getMessage() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("Hello Xtend!");
+    return _builder;
+  }
+}
